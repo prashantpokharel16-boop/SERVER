7f9e7389ae47ab84a3fbcb09244648a569d0a2bb
prashantpokharel16-boop <prashantpokharel16@gmail.com>
2026-05-25 07:46:49 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+```javascript
+window.addEventListener('scroll', function(){
+
+  const nav = document.querySelector('nav');
+
+  if(window.scrollY > 50){
+    nav.style.background = 'rgba(0,0,0,0.85)';
+  }
+  else{
+    nav.style.background = 'rgba(0,0,0,0.4)';
+  }
+
+});
+```
